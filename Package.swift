7e215bc64dc0c0f9aa9739a26f7fe34e924f3b54
IBRAHIM YILMAZ <0ibrahimyilmaz@gmail.com>
2023-06-12 09:26:10 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -8,11 +8,6 @@ let package = Package(
     platforms: [.iOS(.v10)],
     products: [
         .library(
-            name: "IDNowSDKCore-with-NFC",
-            targets: [
-                "IDNowSDKCore-with-NFC", "FaceTecSDK", "XS2AiOSNetService"
-            ]),
-        .library(
             name: "IDNowSDKCore-without-NFC",
             targets: [
                 "IDNowSDKCore-without-NFC", "FaceTecSDK", "XS2AiOSNetService"
@@ -20,21 +15,12 @@ let package = Package(
     ],
     targets: [
         .binaryTarget(
-            name: "IDNowSDKCore-with-NFC",
-            path: "Frameworks/IDNowSDKCore-with-NFC.xcframework"
-        ),
-        .binaryTarget(
             name: "IDNowSDKCore-without-NFC",
             path: "Frameworks/IDNowSDKCore-without-NFC.xcframework"
         ),
         .binaryTarget(
             name: "FaceTecSDK",
             path: "Frameworks/FaceTecSDK.xcframework"
-        ),
-        .binaryTarget(
-            name: "XS2AiOSNetService",
-            url: "https://github.com/FinTecSystems/xs2a-ios-netservice/releases/download/1.0.7/XS2AiOSNetService.xcframework.zip",
-            checksum: "987e1e075fc9ca8b2f22cb60b436b84cc1304bb7c03cbce967979b588e5e8868"
         )
     ],
     swiftLanguageVersions: [.v5]
